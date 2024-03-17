b76153c4037d3dc2d195f37e38c8b9f9478ad602
EbanSnt <negociosebansnt@gmail.com>
2024-03-17 08:45:34 -0300

    Varios
    Se crea las vistas de algunos template, y la view de "create user" y "create school information"

---
+++ home_system/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('home', home, name="home"),
+    path('login',login, name='login'),
+    path('sign_up', sign_up, name='sign_up'),
+    path('permision_dennied', permision_dennied, name='permision_dennied'),
+    path('login_required',permision_dennied_login,name='permision_dennied_login')
+
+]
--- home_system/views.py
+++ home_system/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render,redirect
 from .models import ActionsHistory,Attendances,Grades,SchoolInformation,SchoolStaff,SchoolUsers,SchoolYear,StudentFile,Students,StudentsPayments,Subjects
 import datetime
 
+### TEMPLATES "LOGIN, SIGN UP, HOME Y PERMISSION DENNIED" ###
+
+def login(request):
+    return render(request,"login.html")
+
+
+def sign_up(request):
+    return render(request,"sign_up.html")
+
+
+def home(request):
+    if request.session['active'] == True:
+        return render(request,"home.html")
+    else:
+        return redirect("permision_dennied_login")
+
+
+def permision_dennied(request):
+    return render(request,"permision_dennied.html")
+
+
+def permision_dennied_login(request):
+    return render(request,"permision_dennied_login.html")
+
 ### CREATE USER #### Solo se crea. El admin solo puede modificar y eliminar los datos
 
 def create_user(request):
@@ -13,8 +37,15 @@ def create_user(request):
             email = request.POST["email"]
 
             SchoolUsers.objects.create(dni=dni,username=username,password=password,email=email)
+
+            details = f"Creacion de usuario\nNombre: {username}. DNI: {dni}. Email: {email}"
+
+            ActionsHistory.objects.create(date=datetime.datetime.now(),details=details,action_type="Creacion de Usuario")
+            return redirect("login.html")
+        else:
+            return render(request,"sign_up.html")
     except:
-        pass
+        return render(request,"sign_up.html")
         
 ### Todos los "CREATE"
 
@@ -47,13 +78,16 @@ def create_school_information(request):
 
                     SchoolInformation.objects.create(school_logo=school_logo,name=name,address=address,district=district,locality=locality,telephone_number=telephone_number,telephone_number2=telephone_number2,telephone_number3=telephone_number3,telephone_number4=telephone_number4,email=email,alternative_email=alternative_email,school_type=school_type,facebook=facebook,instagram=instagram,twitter_x=twitter_x,linkedin=linkedin,youtube=youtube,telegram=telegram,whatsapp=whatsapp,web_site=web_site)
 
-                    ActionsHistory.objects.create(date=datetime.datetime.now())
-                    #return redirect()
-                
+                    details = {"name":name,"address":address,"district":district,"locality":locality,"telephone_number":telephone_number,"telephone_number2":telephone_number2,"telephone_number3":telephone_number3,"telephone_number4":telephone_number4,"email":email,"alternative_email":alternative_email,"school_type":school_type,"facebook":facebook,"instagram":instagram,"twitter_x":twitter_x,"linkedin":linkedin,"youtube":youtube,"telegram":telegram,"whatsapp":whatsapp,"web_site":web_site}
+
+                    ActionsHistory.objects.create(date=datetime.datetime.now(),details=details,action_type="Suba de Informacion acerca de la Escuela")
+
+                    return redirect("home")
+                else:
+                    return render(request,"create_school_information.html")
             else:
-                pass      
+                return redirect("permision_dennied_login")      
         else:
-            pass        
+            return redirect("permision_dennied")        
     except:
-        pass
-        #return render(request,)
+        return render(request,"create_school_information.html")
